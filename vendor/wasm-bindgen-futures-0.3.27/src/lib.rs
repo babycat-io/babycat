25689ede9d7f0de4e68cb4d608b89cf580e9a993
James Mishra <james.mishra@neocrym.com>
2021-05-04 20:17:04 -0500

    update vendor

---
+++ vendor/wasm-bindgen-futures-0.3.27/src/lib.rs
@@ -0,0 +1,134 @@
+//! Converting between JavaScript `Promise`s to Rust `Future`s.
+//!
+//! This crate provides a bridge for working with JavaScript `Promise` types as
+//! a Rust `Future`, and similarly contains utilities to turn a rust `Future`
+//! into a JavaScript `Promise`. This can be useful when working with
+//! asynchronous or otherwise blocking work in Rust (wasm), and provides the
+//! ability to interoperate with JavaScript events and JavaScript I/O
+//! primitives.
+//!
+//! There are two main interfaces in this crate currently:
+//!
+//! 1. [**`JsFuture`**](./struct.JsFuture.html)
+//!
+//!    A type that is constructed with a `Promise` and can then be used as a
+//!    `Future<Item = JsValue, Error = JsValue>`. This Rust future will resolve
+//!    or reject with the value coming out of the `Promise`.
+//!
+//! 2. [**`future_to_promise`**](./fn.future_to_promise.html)
+//!
+//!    Converts a Rust `Future<Item = JsValue, Error = JsValue>` into a
+//!    JavaScript `Promise`. The future's result will translate to either a
+//!    rejected or resolved `Promise` in JavaScript.
+//!
+//! These two items should provide enough of a bridge to interoperate the two
+//! systems and make sure that Rust/JavaScript can work together with
+//! asynchronous and I/O work.
+//!
+//! # Example Usage
+//!
+//! This example wraps JavaScript's `Promise.resolve()` into a Rust `Future` for
+//! running tasks on the next tick of the micro task queue. The futures built on
+//! top of it can be scheduled for execution by conversion into a JavaScript
+//! `Promise`.
+//!
+//! ```rust,no_run
+//! extern crate futures;
+//! extern crate js_sys;
+//! extern crate wasm_bindgen;
+//! extern crate wasm_bindgen_futures;
+//!
+//! use futures::{Async, Future, Poll};
+//! use wasm_bindgen::prelude::*;
+//! use wasm_bindgen_futures::{JsFuture, future_to_promise};
+//!
+//! /// A future that becomes ready after a tick of the micro task queue.
+//! pub struct NextTick {
+//!     inner: JsFuture,
+//! }
+//!
+//! impl NextTick {
+//!     /// Construct a new `NextTick` future.
+//!     pub fn new() -> NextTick {
+//!         // Create a resolved promise that will run its callbacks on the next
+//!         // tick of the micro task queue.
+//!         let promise = js_sys::Promise::resolve(&JsValue::NULL);
+//!         // Convert the promise into a `JsFuture`.
+//!         let inner = JsFuture::from(promise);
+//!         NextTick { inner }
+//!     }
+//! }
+//!
+//! impl Future for NextTick {
+//!     type Item = ();
+//!     type Error = ();
+//!
+//!     fn poll(&mut self) -> Poll<(), ()> {
+//!         // Polling a `NextTick` just forwards to polling if the inner promise is
+//!         // ready.
+//!         match self.inner.poll() {
+//!             Ok(Async::Ready(_)) => Ok(Async::Ready(())),
+//!             Ok(Async::NotReady) => Ok(Async::NotReady),
+//!             Err(_) => unreachable!(
+//!                 "We only create NextTick with a resolved inner promise, never \
+//!                  a rejected one, so we can't get an error here"
+//!             ),
+//!         }
+//!     }
+//! }
+//!
+//! /// Export a function to JavaScript that does some work in the next tick of the
+//! /// micro task queue!
+//! #[wasm_bindgen]
+//! pub fn schedule_some_work_for_next_tick() -> js_sys::Promise {
+//!     let future = NextTick::new()
+//!         // Do some work...
+//!         .and_then(|_| {
+//!             Ok(42)
+//!         })
+//!         // And then convert the `Item` and `Error` into `JsValue`.
+//!         .map(|result| {
+//!             JsValue::from(result)
+//!         })
+//!         .map_err(|error| {
+//!             let js_error = js_sys::Error::new(&format!("uh oh! {:?}", error));
+//!             JsValue::from(js_error)
+//!         });
+//!
+//!     // Convert the `Future<Item = JsValue, Error = JsValue>` into a JavaScript
+//!     // `Promise`!
+//!     future_to_promise(future)
+//! }
+//! ```
+
+#![cfg_attr(target_feature = "atomics", feature(stdsimd))]
+#![deny(missing_docs)]
+
+use cfg_if::cfg_if;
+
+mod legacy_shared;
+pub use legacy_shared::*;
+
+cfg_if! {
+    if #[cfg(target_feature = "atomics")] {
+        /// Contains a thread-safe version of this crate, with Futures 0.1
+        mod legacy_atomics;
+        pub use legacy_atomics::*;
+
+        /// Polyfill for `Atomics.waitAsync` function
+        mod wait_async_polyfill;
+    } else {
+        mod legacy;
+        pub use legacy::*;
+     }
+}
+
+#[cfg(feature = "futures_0_3")]
+cfg_if! {
+    if #[cfg(target_feature = "atomics")] {
+        compile_error!("futures 0.3 support is not implemented with atomics yet");
+    } else {
+        /// Contains a Futures 0.3 implementation of this crate.
+        pub mod futures_0_3;
+     }
+}
---
+++ vendor/wasm-bindgen-futures-0.3.27/tests/tests.rs
@@ -0,0 +1,120 @@
+#![cfg(target_arch = "wasm32")]
+
+extern crate futures;
+extern crate js_sys;
+extern crate wasm_bindgen;
+extern crate wasm_bindgen_futures;
+extern crate wasm_bindgen_test;
+
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+use futures::unsync::oneshot;
+use futures::Future;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::{future_to_promise, spawn_local, JsFuture};
+use wasm_bindgen_test::*;
+
+#[wasm_bindgen_test(async)]
+fn promise_resolve_is_ok_future() -> impl Future<Item = (), Error = JsValue> {
+    let p = js_sys::Promise::resolve(&JsValue::from(42));
+    JsFuture::from(p)
+        .map(|x| {
+            assert_eq!(x, 42);
+        })
+        .map_err(|_| unreachable!())
+}
+
+#[wasm_bindgen_test(async)]
+fn promise_reject_is_error_future() -> impl Future<Item = (), Error = JsValue> {
+    let p = js_sys::Promise::reject(&JsValue::from(42));
+    JsFuture::from(p).map(|_| unreachable!()).or_else(|e| {
+        assert_eq!(e, 42);
+        Ok(())
+    })
+}
+
+#[wasm_bindgen_test(async)]
+fn ok_future_is_resolved_promise() -> impl Future<Item = (), Error = JsValue> {
+    let f = futures::future::ok(JsValue::from(42));
+    let p = future_to_promise(f);
+    JsFuture::from(p)
+        .map(|x| {
+            assert_eq!(x, 42);
+        })
+        .map_err(|_| unreachable!())
+}
+
+#[wasm_bindgen_test(async)]
+fn error_future_is_rejected_promise() -> impl Future<Item = (), Error = JsValue> {
+    let f = futures::future::err(JsValue::from(42));
+    let p = future_to_promise(f);
+    JsFuture::from(p).map(|_| unreachable!()).or_else(|e| {
+        assert_eq!(e, 42);
+        Ok(())
+    })
+}
+
+#[wasm_bindgen_test]
+fn debug_jsfuture() {
+    let p = js_sys::Promise::resolve(&JsValue::from(42));
+    let f = JsFuture::from(p);
+    assert_eq!(&format!("{:?}", f), "JsFuture { ... }");
+}
+
+#[wasm_bindgen]
+extern "C" {
+    fn setTimeout(c: &Closure<FnMut()>);
+}
+
+#[wasm_bindgen_test(async)]
+fn oneshot_works() -> impl Future<Item = (), Error = JsValue> {
+    let (tx, rx) = oneshot::channel::<u32>();
+    let mut tx = Some(tx);
+    let closure = Closure::wrap(Box::new(move || {
+        drop(tx.take().unwrap());
+    }) as Box<FnMut()>);
+    setTimeout(&closure);
+    closure.forget();
+    rx.then(|_| Ok(()))
+}
+
+#[wasm_bindgen_test(async)]
+fn spawn_local_runs() -> impl Future<Item = (), Error = JsValue> {
+    let (tx, rx) = oneshot::channel::<u32>();
+    spawn_local(futures::future::ok::<(), ()>(()).map(|_| {
+        tx.send(42).unwrap();
+    }));
+    rx.then(|val| {
+        if val == Ok(42) {
+            Ok(())
+        } else {
+            Err(JsValue::undefined())
+        }
+    })
+}
+
+/// check that `spawn_local` does not forward the `future::err` as an unchecked rejection
+#[wasm_bindgen_test(async)]
+fn spawn_local_err_no_exception() -> impl Future<Item = (), Error = JsValue> {
+    let (tx, rx) = oneshot::channel::<u32>();
+    // Promises should run in a deterministic order, so the `err` should be handled during the execution of this test.
+    spawn_local(futures::future::err::<(), ()>(()));
+    spawn_local(futures::future::ok::<(), ()>(()).map(move |_| {
+        tx.send(42).unwrap();
+    }));
+    rx.then(|val| {
+        if val == Ok(42) {
+            Ok(())
+        } else {
+            Err(JsValue::undefined())
+        }
+    })
+}
+
+#[wasm_bindgen_test(async)]
+fn can_create_multiple_futures_from_same_promise() -> impl Future<Item = (), Error = JsValue> {
+    let promise = js_sys::Promise::resolve(&JsValue::null());
+    let a = JsFuture::from(promise.clone());
+    let b = JsFuture::from(promise);
+    futures::future::join_all(vec![a, b]).map(|_| ())
+}
