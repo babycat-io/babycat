276b8a34c73074ef23894cc288295b64ce87f5d1
James Mishra <james.mishra@neocrym.com>
2021-05-06 23:48:41 -0500

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,9 @@ mod backend;
 #[cfg(feature = "frontend-rust")]
 pub use crate::backend::*;
 
-#[cfg(any(feature = "frontend-c", feature = "frontend-python", feature = "frontend-wasm"))]
+#[cfg(any(
+    feature = "frontend-c",
+    feature = "frontend-python",
+    feature = "frontend-wasm"
+))]
 pub mod frontends;
