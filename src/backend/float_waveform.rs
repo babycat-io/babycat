b2928d17ca09bbc5e72ab200d0a71fe1ed6db76f
James Mishra <james.mishra@neocrym.com>
2021-05-07 20:44:24 -0500

    More Rust doctests

--- src/backend/float_waveform.rs
+++ src/backend/float_waveform.rs
@@ -66,6 +66,25 @@ impl fmt::Debug for FloatWaveform {
 
 impl FloatWaveform {
     /// Constructs a `FloatWaveform` from an already-decoded vector of 32-bit float samples.
+    ///
+    /// # Examples
+    ///
+    /// This creates a `FloatWaveform` containing one second of silent *stereo* audio.
+    /// Note that the input vector contains 88,200 audio samples--which we divide into
+    /// 44,100 frames containing two samples each.
+    /// ```
+    /// use babycat::FloatWaveform;
+    ///
+    /// let frame_rate_hz = 44100;
+    /// let num_channels = 2;
+    /// let raw_uncompressed_audio: Vec<f32> = vec![0.0_f32; 88200];
+    /// let waveform = FloatWaveform::new(frame_rate_hz, num_channels, raw_uncompressed_audio);
+    /// assert_eq!(
+    ///     format!("{:?}", waveform),
+    ///     "FloatWaveform { frame_rate_hz: 44100, num_channels: 2, num_frames: 44100}"
+    /// );
+    /// ```
+    ///
     pub fn new(frame_rate_hz: u32, num_channels: u32, interleaved_samples: Vec<f32>) -> Self {
         let num_frames = interleaved_samples.len() as u64 / num_channels as u64;
         FloatWaveform {
@@ -76,6 +95,19 @@ impl FloatWaveform {
         }
     }
     /// Creates a silent waveform measured in frames.
+    ///
+    /// # Examples
+    /// This creates a `FloatWaveform` containing one second of silent *stereo* audio.
+    /// ```
+    /// use babycat::FloatWaveform;
+    ///
+    /// let waveform = FloatWaveform::from_frames_of_silence(44100, 2, 44100);
+    /// assert_eq!(
+    ///     format!("{:?}", waveform),
+    ///     "FloatWaveform { frame_rate_hz: 44100, num_channels: 2, num_frames: 44100}"
+    /// );
+    /// ```
+    ///
     pub fn from_frames_of_silence(frame_rate_hz: u32, num_channels: u32, num_frames: u64) -> Self {
         FloatWaveform {
             frame_rate_hz,
@@ -85,6 +117,20 @@ impl FloatWaveform {
         }
     }
 
+    /// Create a silent waveform measured in milliseconds.
+    ///
+    /// # Examples
+    /// This creates a `FloatWaveform` containing one second of silent *stereo* audio.
+    /// ```
+    /// use babycat::FloatWaveform;
+    ///
+    /// let waveform = FloatWaveform::from_milliseconds_of_silence(44100, 2, 1000);
+    /// assert_eq!(
+    ///     format!("{:?}", waveform),
+    ///     "FloatWaveform { frame_rate_hz: 44100, num_channels: 2, num_frames: 44100}"
+    /// );
+    /// ```
+    ///
     pub fn from_milliseconds_of_silence(
         frame_rate_hz: u32,
         num_channels: u32,
@@ -370,6 +416,24 @@ impl FloatWaveform {
     }
 
     /// Decodes audio stored in an in-memory byte array.
+    ///
+    /// # Examples
+    /// ```
+    /// use babycat::FloatWaveform;
+    ///
+    /// let encoded_bytes: Vec<u8> = std::fs::read("audio-for-tests/andreas-theme/track.mp3").unwrap();
+    ///
+    /// let decode_args = Default::default();
+    ///
+    /// let waveform = FloatWaveform::from_encoded_bytes(
+    ///     &encoded_bytes,
+    ///     decode_args,
+    /// ).unwrap();
+    /// assert_eq!(
+    ///     format!("{:?}", waveform),
+    ///     "FloatWaveform { frame_rate_hz: 44100, num_channels: 2, num_frames: 9586944}"
+    /// );
+    /// ```
     pub fn from_encoded_bytes(
         encoded_bytes: &[u8],
         decode_args: DecodeArgs,
@@ -457,7 +521,7 @@ impl FloatWaveform {
     ///
     /// # Examples
     /// **(Attempt to) decode three files:**
-    /// 
+    ///
     /// In this example, we process three filenames and demonstrate how to handle errors.
     /// The first two files are successfully processed, and we catch a
     /// [crate::Error::FileNotFound] error when processing the third file.
@@ -542,7 +606,7 @@ impl FloatWaveform {
     ///    format!("{:?}", waveform),
     ///    "FloatWaveform { frame_rate_hz: 44100, num_channels: 2, num_frames: 2492928}"
     /// );
-    /// 
+    ///
     /// let upsampled = waveform.resample(96000).unwrap();
     /// assert_eq!(
     ///    format!("{:?}", upsampled),
