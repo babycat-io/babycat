0dc218b481187c8ba171340ff48fbb5db8eff6f1
James Mishra <james.mishra@neocrym.com>
2021-05-14 12:25:38 -0500

    fmt

--- examples/decode.rs
+++ examples/decode.rs
@@ -4,13 +4,14 @@ fn main() {
     let decode_args = DecodeArgs {
         ..Default::default()
     };
-    let waveform = match FloatWaveform::from_file("audio-for-tests/circus-of-freaks/track.mp3", decode_args) {
-        Ok(w) => w,
-        Err(err) => {
-            println!("Decoding error: {}", err);
-            return;
-        }
-    };
+    let waveform =
+        match FloatWaveform::from_file("audio-for-tests/circus-of-freaks/track.mp3", decode_args) {
+            Ok(w) => w,
+            Err(err) => {
+                println!("Decoding error: {}", err);
+                return;
+            }
+        };
     println!(
         "Decoded {} frames with {} channels at {} hz",
         waveform.num_frames(),
