f283747d2c4cbbe5383a9ee0f4d2c4f28a097793
James Mishra <james.mishra@neocrym.com>
2021-05-15 21:52:31 -0500

    babycat.io: Switch to a large card on Twitter

--- docs/babycat.io/source/conf.py
+++ docs/babycat.io/source/conf.py
@@ -44,6 +44,10 @@ exclude_patterns: List[str] = []
 
 ogp_image = "https://static.neocrym.com/images/babycat/v1/1x/babycat-body-icon-dark-social-media-cover--1x.png"
 
+ogp_custom_meta_tags = [
+    '<meta name="twitter:card" content="summary_large_image" />',
+    '<meta property="twitter:image" content="https://static.neocrym.com/images/babycat/v1/1x/babycat-body-icon-dark-social-media-cover--1x.png" />',
+]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
