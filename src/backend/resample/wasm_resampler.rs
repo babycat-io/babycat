6b27203ddfd8f07905d516a8102d26881a31e4e1
Ritik Mishra <ritik.mishra314@gmail.com>
2021-06-01 00:24:31 -0500

    use resampy "kaiser_best" filter, improve some variable names
    idk how resampy generated this filter. i just took whatever they had packaged and put it in a txt file.
    
    also there are some issues with the floating point stuff, so i changed some of the accounting-type variables to f64

--- src/backend/resample/wasm_resampler.rs
+++ src/backend/resample/wasm_resampler.rs
@@ -1,4 +1,6 @@
-use crate::{backend::errors::Error, resample::common::validate_args};
+use crate::{backend::errors::Error, resample::common::{get_num_output_frames, validate_args}};
+
+const KAISER_BEST_WINDOW: [f32; 32769] = include!("kaiser_best.txt");
 
 fn sinc(x: f32) -> f32 {
     if x == 0.0 {
@@ -19,10 +21,13 @@ pub fn resample(
 
     let sample_ratio: f32 = (output_frame_rate_hz as f32) / (input_frame_rate_hz as f32);
 
-    let ret_size: usize = ((input_audio.len() as f32) * sample_ratio).ceil() as usize;
+    let num_output_frames = get_num_output_frames(input_audio, input_frame_rate_hz, output_frame_rate_hz, num_channels);
+    let ret_size = num_output_frames * (num_channels as usize);
     let mut ret: Vec<f32> = vec![0.0_f32; ret_size];
 
-    let (mut interp_win, precision, _) = sinc_window();
+    // let (mut interp_win, precision, _) = sinc_window();
+    let mut interp_win = KAISER_BEST_WINDOW.clone();
+    let precision = 512;
     if sample_ratio < 1.0 {
         for i in interp_win.iter_mut() {
             *i *= sample_ratio;
@@ -40,7 +45,7 @@ pub fn resample(
         input_audio,
         &mut ret,
         num_channels as usize,
-        sample_ratio,
+        (output_frame_rate_hz as f64) / (input_frame_rate_hz as f64),
         &interp_win,
         &interp_delta,
         precision,
@@ -53,54 +58,57 @@ fn resample_f(
     in_audio: &[f32],
     out_audio: &mut [f32],
     num_channels: usize,
-    sample_ratio: f32,
+    sample_ratio: f64,
     interp_win: &[f32],
     interp_delta: &[f32],
     num_table: i32,
 ) {
-    let scale = std::primitive::f32::min(sample_ratio, 1.0);
+    let scale = std::primitive::f64::min(sample_ratio, 1.0);
 
     // equal to (in_audio_hz / out_audio_hz)
     let time_increment = 1.0 / sample_ratio;
-    let index_step = (scale * num_table as f32) as usize;
+    let index_step = (scale * num_table as f64) as usize;
 
     let n_win = interp_win.len();
     let n_in_frames = in_audio.len() / (num_channels);
     let n_out_frames = out_audio.len() / (num_channels);
 
     for out_frame_idx in 0..n_out_frames {
-        let time_register = time_increment * out_frame_idx as f32;
+        let time_register = time_increment * out_frame_idx as f64;
         let in_frame_idx = time_register as usize;
 
-        let frac: f32 = scale * time_register.fract();
-        let index_frac = frac * num_table as f32;
+        let frac: f64 = scale * time_register.fract();
+        let index_frac = frac * num_table as f64;
         let offset = index_frac as usize;
-        let eta = index_frac.fract();
+        let eta = index_frac.fract() as f32;
 
         let i_max = std::cmp::min(in_frame_idx + 1, (n_win - offset) / index_step);
         for i in 0..i_max {
             let weight =
                 interp_win[offset + i * index_step] + eta * interp_delta[offset + i * index_step];
-            for j in 0..num_channels {
-                let out_idx: usize = (out_frame_idx * num_channels) + j;
-                let in_idx: usize = ((in_frame_idx - i) * num_channels) + j;
+            for channel_idx in 0..num_channels {
+                let out_idx: usize = (out_frame_idx * num_channels) + channel_idx;
+                let in_idx: usize = ((in_frame_idx - i) * num_channels) + channel_idx;
                 out_audio[out_idx] += weight * in_audio[in_idx];
             }
         }
 
         // Right wing of response
         let frac = scale - frac;
-        let index_frac = frac * num_table as f32;
+        let index_frac = frac * num_table as f64;
         let offset = index_frac as usize;
-        let eta = index_frac.fract();
+        let eta = index_frac.fract() as f32;
 
-        let k_max = std::cmp::min(n_in_frames - in_frame_idx - 1, (n_win - offset) / index_step);
+        let k_max = std::cmp::min(
+            n_in_frames - in_frame_idx - 1,
+            (n_win - offset) / index_step,
+        );
         for k in 0..k_max {
             let weight =
                 interp_win[offset + k * index_step] + eta * interp_delta[offset + k * index_step];
-            for j in 0..num_channels {
-                let out_idx: usize = (out_frame_idx * num_channels) + j;
-                let in_idx: usize = ((in_frame_idx + k + 1) * num_channels) + j;
+            for channel_idx in 0..num_channels {
+                let out_idx: usize = (out_frame_idx * num_channels) + channel_idx;
+                let in_idx: usize = ((in_frame_idx + k + 1) * num_channels) + channel_idx;
                 out_audio[out_idx] += weight * in_audio[in_idx];
             }
         }
@@ -171,7 +179,6 @@ mod test {
 
         for (i, (actual, expected)) in actual_results.zip(expected_results).enumerate() {
             let error = (actual - expected).abs();
-            eprintln!("{} error: {}", i, error);
             assert!(error < EPSILON)
         }
     }
