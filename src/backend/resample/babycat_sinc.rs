3729b9f9d57ba90efaecc4ef623635f9a195ca68
James Mishra <james.mishra@neocrym.com>
2021-07-11 11:15:11 -0500

    Remove sinc and blackman harris computations.
    Instead of computing sinc values, we are going to rely on
    our precomputed table.

--- src/backend/resample/babycat_sinc.rs
+++ src/backend/resample/babycat_sinc.rs
@@ -13,15 +13,6 @@ use crate::backend::{
 #[allow(clippy::excessive_precision)]
 const KAISER_BEST_WINDOW: [f32; 32769] = include!("kaiser_best.txt");
 
-fn sinc(x: f32) -> f32 {
-    if float_cmp::approx_eq!(f32, x, 0.0_f32) {
-        1.0
-    } else {
-        let k = x * std::f32::consts::PI;
-        std::primitive::f32::sin(k) / k
-    }
-}
-
 pub fn resample(
     input_frame_rate_hz: u32,
     output_frame_rate_hz: u32,
@@ -41,7 +32,6 @@ pub fn resample(
     let ret_size = num_output_frames * (num_channels as usize);
     let mut ret: Vec<f32> = vec![0.0_f32; ret_size];
 
-    // let (mut interp_win, precision, _) = sinc_window();
     let mut interp_win = KAISER_BEST_WINDOW;
     let precision = 512;
     if sample_ratio < 1.0 {
@@ -127,75 +117,6 @@ fn resample_f(
                 let in_idx: usize = ((in_frame_idx + k + 1) * num_channels) + channel_idx;
                 out_audio[out_idx] += weight * in_audio[in_idx];
             }
-        }
-    }
-}
-
-pub fn sinc_window() -> (Vec<f32>, i32, f32) {
-    let num_zeros = 512;
-    let precision = 9;
-    let rolloff = 0.945;
-
-    let num_bits = 1 << precision;
-    let n = num_bits * num_zeros;
-
-    let sinc_win = (0..=n)
-        .map(|i| (num_zeros as f32) * ((i as f32) / (n as f32)) * (num_zeros as f32)) // np.linspace
-        .map(|x| rolloff * sinc(x * rolloff)); // rolloff * np.sinc;
-
-    let taper = blackman_harris(2 * n + 1).skip(n as usize);
-
-    let interp_win = taper.zip(sinc_win).map(|(t, s)| t * s).collect::<Vec<_>>();
-
-    (interp_win, num_bits, rolloff)
-}
-
-pub fn blackman_harris(n: i32) -> impl Iterator<Item = f32> {
-    const C1: f32 = 0.35875;
-    const C2: f32 = 0.48829;
-    const C3: f32 = 0.14128;
-    const C4: f32 = 0.01168;
-
-    const PI_2: f32 = std::f32::consts::TAU;
-    const PI_4: f32 = std::f32::consts::TAU * 2.0;
-    const PI_6: f32 = std::f32::consts::PI * 6.0;
-
-    let aaa: f32 = n as f32 - 1.0;
-    let v2 = PI_2 / aaa;
-    let v3 = PI_4 / aaa;
-    let v4 = PI_6 / aaa;
-
-    (0..n).map(move |k| {
-        C1 - C2 * (v2 * k as f32).cos() + C3 * (v3 * k as f32).cos() - C4 * (v4 * k as f32).cos()
-    })
-}
-
-#[cfg(test)]
-mod test {
-    use super::blackman_harris;
-
-    #[test]
-    fn test_blackman_harris() {
-        // expected_results generated from scipy.signal.blackmanharris
-        let expected_results: Vec<f32> = vec![
-            6.0000000000001025e-05,
-            0.015071173410218162,
-            0.1470395578623815,
-            0.5205749999999999,
-            0.9316592687274005,
-            0.9316592687274005,
-            0.5205750000000002,
-            0.14703955786238157,
-            0.015071173410218162,
-            6.0000000000001025e-05,
-        ];
-        let actual_results = blackman_harris(10);
-
-        const EPSILON: f32 = 1e-6;
-
-        for (_i, (actual, expected)) in actual_results.zip(expected_results).enumerate() {
-            let error = (actual - expected).abs();
-            assert!(error < EPSILON)
         }
     }
 }
